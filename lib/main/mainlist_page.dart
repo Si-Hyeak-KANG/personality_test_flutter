951d5513cff987e4e6e97bbf9307b191d710a55e
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-09-25 22:38:07 +0900

    firebase database 연동 및 데이터 조회

--- lib/main/mainlist_page.dart
+++ lib/main/mainlist_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:firebase_database/firebase_database.dart';
 import '../sub/question_page.dart';
 
 final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
@@ -22,6 +23,9 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPage extends State<MainPage> {
+  FirebaseDatabase database = FirebaseDatabase.instance;
+  late DatabaseReference _testRef;
+  late List<String> testList = List.empty(growable: true);
 
   String welcomeTitle = '';
   bool bannerUse = false;
@@ -31,6 +35,7 @@ class _MainPage extends State<MainPage> {
   void initState() {
     super.initState();
     remoteConfigInit();
+    _testRef = database.ref('test');
   }
 
   void remoteConfigInit() async {
@@ -43,8 +48,12 @@ class _MainPage extends State<MainPage> {
     print('itemHeight : $itemHeight');
   }
 
-  Future<String> loadAsset() async {
-    return await rootBundle.loadString('res/api/list.json');
+  Future<List<String>> loadAsset() async {
+    // return await rootBundle.loadString('res/api/list.json'); // 로컬 json 호출
+    await _testRef.get().then((value) => value.children.forEach((element) {
+      testList.add(element.value.toString());
+    }));
+    return testList;
   }
   @override
   Widget build(BuildContext context) {
@@ -69,40 +78,88 @@ class _MainPage extends State<MainPage> {
               );
             case ConnectionState.done:
               if (snapshot.hasData) {
-                Map<String, dynamic> list = jsonDecode(snapshot.data!);
+                // Map<String, dynamic> list = jsonDecode(snapshot.data!);
                 return ListView.builder(
+                  itemCount: snapshot.data!.length,
                   itemBuilder: (context, value) {
+                    Map<String, dynamic> item = jsonDecode(snapshot.data![value]);
                     return InkWell(
+                      child: SizedBox(
+                        height: remoteConfig.getInt("item_height").toDouble(),
+                        child: Card(
+                          color: Colors.amber,
+                          child: Text(item['title'].toString()),
+                        ),
+                      ),
                       onTap: () async {
                         await FirebaseAnalytics.instance.logEvent(
                           name: "test_click",
                           parameters: {
-                            "test_name" :
-                                list['questions'][value]['title'].toString(),
+                            "test_name" : item['title'].toString(),
                           },
                         ).then((result) {
                           Navigator.of(context)
                               .push(MaterialPageRoute(builder: (context) {
-                                return QuestionPage(
-                                  question: list['questions'][value]['file'].toString(),
-                                );
+                              return QuestionPage(question: item);
                           }));
                         });
                       },
-                      child: SizedBox(
-                        height: itemHeight.toDouble(),
-                        child: Card(
-                          child: Text(list['questions'][value]['title'].toString()),
-                        ),
-                      ),
                     );
                   },
-                  itemCount: list['count'],
                 );
               } else {
                 return Text('Error: ${snapshot.error}');
               }
           }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseDatabase database = FirebaseDatabase.instance;
+          DatabaseReference _testRef = database.ref('test');
+          _testRef.push().set("""{
+            "title":"당신이 좋아하는 애완동물은?",
+            "question":"당신이 무인도에 도착했는데, 마침 떠내려온 상자를 열었을 때 보이는 이것은?",
+            "selects": [
+              "생존 키트",
+              "휴대폰",
+              "텐트",
+              "무인도에서 살아남기"
+            ],
+            "answer":[
+              "당신은 현실주의! 동물은 안키운다!!",
+              "당신은 늘 함께 있는 걸 좋아하는 강아지",
+              "당신은 같은 공간을 공유하는 고양이",
+              "당신은 낭만을 좋아하는 앵무새"
+            ]
+          }""");
+          _testRef.push().set("""{
+            "title":"5초 MBTI I/E 편",
+            "question":"친구와 함께 간 미술관 당신이라면?",
+            "selects":[
+              "말이 많아짐",
+              "생각이 많아짐"
+            ],
+            "answer":[
+              "당신의 성향은 E",
+              "당신의 성향은 I"
+            ]
+          }""");
+          _testRef.push().set("""{
+            "title":"당신은 어떤 사랑을 하고 싶나요?",
+            "question":"목욕을 할 때 가장 먼저 비누칠을 하는 곳은?",
+            "selects":[
+              "머리",
+              "상체",
+              "하체"
+            ],
+            "answer":[
+              "당신은 자만추를 추천해요.",
+              "당신은 소개팅에서 새로운 사람을 소개받는 걸 좋아합니다.",
+              "당신은 길 가다가 우연히 지나친 그런 인연을 좋아합니다."
+            ]
+          }""");
         },
       ),
     );
--- lib/sub/question_page.dart
+++ lib/sub/question_page.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 import '../detail/detail_page.dart';
 
 class QuestionPage extends StatefulWidget {
-  final String question;
+  final Map<String, dynamic> question;
   const QuestionPage({super.key, required this.question});
   @override
   State<StatefulWidget> createState() {
@@ -18,9 +18,9 @@ class _QuestionPage extends State<QuestionPage> {
   String title = '';
   int selectNumber = -1;
 
-  Future<String> loadAsset(String fileName) async {
-    return await rootBundle.loadString('res/api/$fileName.json');
-  }
+  // Future<String> loadAsset(String fileName) async {
+  //   return await rootBundle.loadString('res/api/$fileName.json');
+  // }
 
   @override
   void initState() {
@@ -29,79 +29,57 @@ class _QuestionPage extends State<QuestionPage> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: loadAsset(widget.question),
-      builder: (context, snapshot) {
-        if (snapshot.hasData==false) {
-          return const CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-                child: Text(
-              'Error: ${snapshot.error}',
-              style: const TextStyle(fontSize: 15),
-            )),
-          );
-        } else {
-          Map<String, dynamic> questions = jsonDecode(snapshot.data!);
-          title = questions['title'].toString();
-          List<Widget> widgets;
-
-          widgets = List<Widget>.generate(
-              (questions['selects'] as List<dynamic>).length,
-              (int index) => SizedBox(
-                  height: 100,
-                  child: Column(
-                    children: [
-                      Text(questions['selects'][index]),
-                      Radio(
-                          value: index,
-                          groupValue: selectNumber,
-                          onChanged: (value) {
-                            setState(() {
-                              selectNumber = index;
-                            });
-                          })
-                    ],
-                  )));
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(title),
-            ),
-            body: Column(
-              children: [
-              Text(questions['question'].toString()),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: widgets.length,
-                  itemBuilder: (context, index) {
-                    final item = widgets[index];
-                    return item;
-                  },
-                ),
-              ),
-              selectNumber == -1
-                  ? Container()
-                  : ElevatedButton(
-                    onPressed: () async {
-                      await FirebaseAnalytics.instance.logEvent(
-                        name: "personal_select",
-                        parameters: {"test_name":title, "select": selectNumber},
-                      ).then((result) => {
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(builder: (context) {
-                            return DetailPage(
-                              answer: questions['answer'][selectNumber],
-                              question: questions['question']);
-                          }))
-                      });
-                    }, child: const Text('성격보기'))
-              ],
-            ),
-          );
-        }
-      },
+    List<Widget> widgets = List<Widget>.generate(
+        (widget.question['selects'] as List<dynamic>).length,
+        (int index) => SizedBox(
+          height: 100,
+          child: Column(
+            children: [
+              Text(widget.question['selects'][index]),
+              Radio(
+                value: index,
+                groupValue: selectNumber,
+                onChanged: (value) {
+                  selectNumber = index;
+                })
+            ],
+          ),
+        )
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+        Text(widget.question['question'].toString()),
+        Expanded(
+          child: ListView.builder(
+            itemCount: widgets.length,
+            itemBuilder: (context, index) {
+              final item = widgets[index];
+              return item;
+            },
+          ),
+        ),
+        selectNumber == -1
+            ? Container()
+            : ElevatedButton(
+              onPressed: () async {
+                await FirebaseAnalytics.instance.logEvent(
+                  name: "personal_select",
+                  parameters: {"test_name":title, "select": selectNumber},
+                ).then((result) => {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) {
+                      return DetailPage(
+                        answer: widget.question['answer'][selectNumber],
+                        question: widget.question['question']);
+                    }))
+                });
+              }, child: const Text('성격보기'))
+        ],
+      ),
     );
   }
 }
